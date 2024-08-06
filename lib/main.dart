7e6d5d7ca5de493dadfb69d786fc78c922d06148
Nahaq789 <naha022677@icloud.com>
2024-08-07 08:18:16 +0900

    feat: ProviderScopeでラップする

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:subscribe/screens/login_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env.dev');
 
-  runApp(const SubscribeApp());
+  runApp(const ProviderScope(
+    child: SubscribeApp(),
+  ));
 }
 
 class SubscribeApp extends StatelessWidget {
@@ -14,6 +17,7 @@ class SubscribeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Subscribe App",
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: const Color(0xFF1A1F24),
