997c0dce7006f036694b72ef77e06cca82d0b0d3
Nahaq789 <naha022677@icloud.com>
2024-09-06 07:56:19 +0900

    fix: reppository path #3

--- lib/infrastructure/provider/auth_repository_provider.dart
+++ lib/infrastructure/provider/auth_repository_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:subscribe/adapter/firebase/provider/crashlytics_provider.dart';
 import 'package:subscribe/domain/repository/auth_i_repository.dart';
-import 'package:subscribe/infrastructure/auth_repository.dart';
+import 'package:subscribe/infrastructure/repository/auth_repository.dart';
 
 final authRepositoryProvider = Provider<IAuthRepository>((ref) {
   final crashlyticsService = ref.watch(carshlyticsProvider);
