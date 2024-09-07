d314f5102b2cdb6b53c83dc2eb7f456b57fe0896
Nahaq789 <naha022677@icloud.com>
2024-09-08 00:01:24 +0900

    fix: focus #3

--- lib/presentation/components/password_field.dart
+++ lib/presentation/components/password_field.dart
@@ -9,15 +9,16 @@ class CustomInputField extends ConsumerStatefulWidget {
   final bool isPassword;
   final TextEditingController textController;
   final ValueChanged<String> onChanged;
+  final Function(String?)? varidator;
 
-  const CustomInputField({
-    super.key,
-    required this.labelText,
-    required this.hintText,
-    required this.isPassword,
-    required this.textController,
-    required this.onChanged,
-  });
+  const CustomInputField(
+      {super.key,
+      required this.labelText,
+      required this.hintText,
+      required this.isPassword,
+      required this.textController,
+      required this.onChanged,
+      this.varidator});
 
   @override
   CustomInputFieldState createState() => CustomInputFieldState();
@@ -25,6 +26,21 @@ class CustomInputField extends ConsumerStatefulWidget {
 
 class CustomInputFieldState extends ConsumerState<CustomInputField> {
   bool _isObscure = true;
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,15 +55,22 @@ class CustomInputFieldState extends ConsumerState<CustomInputField> {
     final double verticalPadding = isTablet ? 20.0 : 15.0;
     final double horizontalPadding = isTablet ? 20.0 : 15.0;
 
+    debugPrint(_focusNode.hasFocus.toString());
+
     return TextField(
       controller: widget.textController,
       obscureText: widget.isPassword ? _isObscure : false,
+      focusNode: _focusNode,
       onChanged: widget.onChanged,
       style: TextStyle(fontSize: labelFontSize),
       decoration: InputDecoration(
         floatingLabelBehavior: FloatingLabelBehavior.always,
         labelText: widget.labelText,
-        labelStyle: TextStyle(fontSize: labelFontSize),
+        labelStyle: TextStyle(
+            fontSize: labelFontSize,
+            color: _focusNode.hasFocus
+                ? const Color(0xFF9489F5)
+                : const Color(0xFFC0C0C0)),
         hintText: widget.hintText,
         hintStyle: TextStyle(fontSize: hintFontSize),
         border: OutlineInputBorder(
