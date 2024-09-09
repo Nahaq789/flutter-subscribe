0e13847979ba3569a93d746ae12d08723557a7b3
Nahaq789 <naha022677@icloud.com>
2024-09-10 08:14:27 +0900

    fix: return response #3

--- lib/domain/repository/auth_i_repository.dart
+++ lib/domain/repository/auth_i_repository.dart
@@ -3,6 +3,6 @@ import 'package:subscribe/domain/models/auth_user_model.dart';
 
 abstract interface class IAuthRepository {
   Future<Response> authenticate({required AuthUserModel model});
-  Future registerAccount({required AuthUserModel model});
+  Future<Response> registerAccount({required AuthUserModel model});
   Future<Response> confirmCode({required AuthUserModel model});
 }
